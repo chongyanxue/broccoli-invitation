e21f8b9403bb37f622d1d61e71786e774875b5d5
chongyanxue <chongyanxue@qq.com>
2023-08-14 14:52:58 +0800

    Modify test title for app page

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('renders app main page', () => {
   render(<App />);
   const linkElement = screen.getByText(/Be the first to know when we launch./i);
   expect(linkElement).toBeInTheDocument();
